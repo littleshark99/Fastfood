a556b755ac589911f95fb23d8608362fae082bde
littleshark99 <rafael.clare@hotmail.com>
2025-07-23 18:31:10 -0300

    rota criada para filtrar a categoria dos lanches

--- LanchesMac/LanchesMac/Startup.cs
+++ LanchesMac/LanchesMac/Startup.cs
@@ -57,16 +57,12 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapControllerRoute(
-              name: "home",
-              pattern: "{home}",
-              defaults: new { controller = "Home", action = "index" }
-              );
+
 
             endpoints.MapControllerRoute(
-                name: "admin",
-                pattern: "admin",
-                defaults: new { controller = "admin", Action = "index" }
+                name: "categoriaFiltro",
+                pattern: "Lanche/{action}/{categoria}",
+                defaults: new { controller = "Lanche", Action = "List" }
                 );
 
 
